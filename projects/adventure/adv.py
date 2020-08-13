9e51d74445daf27ea91e74cdd742129853a48d79
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-13 15:39:07 -0700

    passing test_loop

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from copy import copy
 
 import random
 from ast import literal_eval
@@ -10,12 +11,12 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt" #pass 3 moves
-map_file = "maps/test_cross.txt" #pass 15 moves
+#map_file = "maps/test_line.txt" #pass 2 moves
+#map_file = "maps/test_cross.txt" #pass 14 moves
 
 
-#map_file = "maps/test_loop.txt" #fail, infinite loop
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop.txt" #pass 14 moves
+#map_file = "maps/test_loop_fork.txt"
 #map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -32,7 +33,8 @@ player = Player(world.starting_room)
 #TODO: track visited rooms, add to traversal path as appropriate
 
 #init
-traversal_path = [0]
+traversal_path = []
+forks = {}
 visited = set()
 current_room = player.current_room
 visited.add(current_room)
@@ -61,63 +63,72 @@ def first_visit():
 
     return False
 
-def first_available():
-    east_room = current_room.get_room_in_direction("e")
-    west_room = current_room.get_room_in_direction("w")
-    north_room = current_room.get_room_in_direction("n")
-    south_room = current_room.get_room_in_direction("s")
-
-    if east_room != None:
-        return "e"
-    elif west_room != None:
-        return "w"
-    elif north_room != None:
-        return "n"
-    elif south_room != None:
+def reverse_dir(dir):
+    if dir == "n":
         return "s"
-    Exception("dead end")
-    return False
+    elif dir == "s":
+        return "n"
+    elif dir == "e":
+        return "w"
+    else:
+        return "e"
+
+def return_to_fork():
+    """travel back to the last fork by reverse-traversing `traversal_path` until current_room == fork"""
+    #get the last fork in the dict (python 3.6+ has ordered dicts by default)
+    last_fork = list(forks.items())[-1][0]
+    #copy the traversal list so we can modify it to efficiently get back to the last fork
+    temp_traverse = copy(traversal_path)
+    while current_room.id != last_fork.id:
+        print(f"returning to fork at {last_fork.id}. currently at {current_room.id}")
+        dir = reverse_dir(temp_traverse.pop())
+        travel(dir)
+    if first_visit():
+        travel(first_visit())
+    else:
+        travel(forks[last_fork][0])
+
+def first_available():
+    return current_room.get_exits()[0]
 
 def travel(dir):
     global current_room
+    #room before travelling
+    if current_room in forks:
+        if dir in forks[current_room]:
+            forks[current_room].remove(dir)
+        if forks[current_room] == []:
+            forks[current_room] = None
+
     player.travel(dir)
     traversal_path.append(dir)
-    current_room = player.current_room
+    current_room = player.current_room    
     if current_room not in visited:
         visited.add(current_room)
+        #add current room to forks if it has more than one exit
+        exits = current_room.get_exits()
+        if len(exits) > 1:
+            forks[current_room] = exits
 
-def travel_back(dir):
-    room = current_room.get_room_in_direction(dir)
-    if room:
-        travel(dir)
-    else:
-        #TODO: Travel back to last fork
-        travel(first_available())
-
-
-def backtrack(from_dir):
+def backtrack():
     while first_visit() == False:
-        if from_dir == "n":
-            travel_back("s")
-        if from_dir == "s":
-            travel_back("n")
-        if from_dir == "e":
-            travel_back("w")
-        if from_dir == "w":
-            travel_back("e")
+        return_to_fork()
     return first_visit()
 
 # MARK: Run Loop
-while len(visited) < len(room_graph):
+while len(visited) < len(room_graph):    
     visit = first_visit()
+    print(f"step: {len(traversal_path)}")
+    print(f"I'm going {visit}")
     if visit:
         travel(visit)
     else:
         #TODO: Backtrack to last node where there was a choice
         while not visit:
-            backtrack(traversal_path[-1])
+            backtrack()
             visit = first_visit()
-        #print("I'm stuck")
+    
+        print(f"I'm stuck at {current_room.id}")
 
 
 # MARK: TRAVERSAL TEST
