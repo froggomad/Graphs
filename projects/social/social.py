017334d49f59eb2e8b46b0ca1e6d52cff29e3545
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-12 17:57:22 -0700

    implemented get_all_social_paths

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,24 @@
+import random
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 class User:
     def __init__(self, name):
         self.name = name
+    
+    def __repr__(self):
+        return f'User({repr(self.name)})'
 
 class SocialGraph:
     def __init__(self):
@@ -28,6 +46,11 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    def reset(self):
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
@@ -39,14 +62,23 @@ class SocialGraph:
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
-        self.last_id = 0
-        self.users = {}
-        self.friendships = {}
-        # !!!! IMPLEMENT ME
+        self.reset()
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i}")
 
         # Create friendships
+        possible_friendships = []
+
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        random.shuffle(possible_friendships)
+
+        for i in range(num_users * avg_friendships //2):
+            friendships = possible_friendships[i]
+            self.add_friendship(friendships[0], friendships[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -56,15 +88,36 @@ class SocialGraph:
         extended network with the shortest friendship path between them.
 
         The key is the friend's ID and the value is the path.
-        """
+
+        ex:
+        {1: {2, 4}, 2: {1, 3}, 3: {2, 4} 4: {1, 3} }
+
+        how does 1 get to 3?
+        """        
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        #init stack with connection to self ([self])
+        stack = Stack()
+        stack.push([user_id])
+        visited[user_id] = [user_id]
+        while stack.size() > 0:
+            #get last friend in friendships
+            friendships = stack.pop()
+            friend = friendships[-1]
+            #get connection to each friend
+            for outside_friend in self.friendships[friend]:
+                if outside_friend not in visited:
+                    connection = list(friendships)
+                    connection.append(outside_friend)
+                    visited[outside_friend] = connection
+                    stack.push(connection)
+
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
-    print(sg.friendships)
+    #print(sg.users)
+    print(sg.friendships)    
     connections = sg.get_all_social_paths(1)
-    print(connections)
+    print(f"connections: {connections}")
