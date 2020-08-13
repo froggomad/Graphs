f16aa4ed4aff34704ecabcf367b910cc88b22aea
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-08-13 13:18:44 -0700

    updated room repr

--- projects/adventure/room.py
+++ projects/adventure/room.py
@@ -11,6 +11,8 @@ class Room:
         self.w_to = None
         self.x = x
         self.y = y
+    def __repr__(self):
+        return f"{self.id}"
     def __str__(self):
         return f"\n-------------------\n\n{self.name}\n\n   {self.description}\n\n{self.get_exits_string()}\n"
     def print_room_description(self, player):
